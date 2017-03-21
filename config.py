3157268f3aeedea22c5d449e53cf0fbba1c1e406
alexako <akosialexpo@gmail.com>
2017-03-21 20:46:16 +0800

    Add configuration file

---
+++ config.py
@@ -0,0 +1,15 @@
+client_id = ""
+client_secret = ""
+username = ""
+password = ""
+subreddit = ""
+user_agent = "PreviewSiteBot v1.0 for /r/%s, by /u/kludgebot" \
+            % (subreddit)
+
+#Keywords to trigger the bot
+keywords = ["!preview", "!previewbot"]
+
+#Screenshotlayer credentials - http://screenshotlayer.com
+access_key = ''
+secret_keyword = ''
+
