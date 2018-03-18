bfb7d6b71ad4036ce6cc9adc3ef3a576509167be
Alex <akosialexpo@gmail.com>
2018-03-18 23:43:05 +0800

    Add environment variables

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-client_id = ""
-client_secret = ""
+import os
+
+client_id = os.environ["CLIENT_ID"]
+client_secret = os.environ["CLIENT_SECRET"]
 username = ""
 password = ""
 subreddit = "all"
@@ -10,6 +12,6 @@ user_agent = "PreviewSiteBot v1.2 for /r/%s, by /u/kludgebot" \
 keywords = ["!preview", "!previewbot", "!previewsite", "!previewsitebot"]
 
 #Screenshotlayer credentials - http://screenshotlayer.com
-access_key = ''
-secret_keyword = ''
+access_key = os.environ["ACCESS_KEY"]
+secret_keyword = os.environ["SECRET_KEYWORD"]
 
